a9991232198eccc5b260cf2a16bab0facf00f38d
toannguyen2603 <huutoan2603@gmail.com>
2021-10-02 15:57:07 +0700

    Add file index.js & fix resposive

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var list = document.querySelectorAll(".navi");
+var itemBox = document.querySelectorAll(".card-list")
+
+for(let i = 0; i < list.length; i++){
+    list[i].addEventListener('click', function() {
+            for(let j = 0; j < list.length; j++){
+                list[j].classList.remove("active");
+        }
+        this.classList.add("active");
+
+        let dataFilter = this.getAttribute("data-filter");
+
+        for (let k = 0; k < itemBox.length; k++) 
+        {
+            itemBox[k].classList.remove("active");
+            itemBox[k].classList.add("hide");
+            console.log(dataFilter, itemBox[k].getAttribute("data-item"));
+
+            if (itemBox[k].getAttribute("data-item") == dataFilter || dataFilter == "*") 
+            {
+            itemBox[k].classList.remove("hide");
+            itemBox[k].classList.add("active");
+            }
+        }
+    });
+}
